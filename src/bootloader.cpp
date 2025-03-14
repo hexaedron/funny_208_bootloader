2cd19fb3d55bd68ccf6383fcd8afe6b1b13dadc0
Konstantin Gorbenko <kyb.6.granei@yandex.ru>
2025-03-15 01:29:47 +0300

    Works

--- src/bootloader.cpp
+++ src/bootloader.cpp
@@ -1,17 +1,28 @@
 #include <ch32fun.h>
 #include <ch32v20xhw.h>
 
-#define MAIN_CODE_ADDR 0x400;
+#define MAIN_CODE_ADDR (0x400);
 
 int main()
 { 
     SystemInit();
 
-    void (*jump_func)(void);
+    SysTick->SR   = 0;
+    SysTick->CMP  = DELAY_MS_TIME; // 1 ms
+    SysTick->CNT  = 0; 
+    SysTick->CTLR |= SYSTICK_CTLR_STE | SYSTICK_CTLR_STIE | SYSTICK_CTLR_STCLK ;
+
+    funGpioInitC();
+    funPinMode(PC0, GPIO_Speed_50MHz | GPIO_CNF_OUT_PP);
+	funPinMode(PC1, GPIO_Speed_50MHz | GPIO_CNF_OUT_PP);
+    Delay_Ms(3000);
+
+    void (*jump_func)(void) ;
 
     jump_func = (void (*)(void))MAIN_CODE_ADDR;
 
-    __disable_irq();
+    //__disable_irq();
+    //asm volatile(" j 0x400;\n");
     jump_func(); 
 }
 
