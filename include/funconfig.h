9eff6bf055e7ed869348adde06e66b3f89db6c26
Konstantin Gorbenko <kyb.6.granei@yandex.ru>
2025-03-14 18:24:45 +0300

    First commit

---
+++ include/funconfig.h
@@ -0,0 +1,15 @@
+#ifndef _FUNCONFIG_H
+#define _FUNCONFIG_H
+
+#define CH32V20x                 1
+#define CH32V208 1
+#define CH32V20x_D8W 1
+#define FUNCONF_SYSTICK_USE_HCLK 1
+#define FUNCONF_USE_HSI          1
+#define FUNCONF_SYSTEM_CORE_CLOCK 120000000
+#define FUNCONF_ENABLE_HPE 1 
+#define FUNCONF_TINYVECTOR 1 
+
+#define FUNCONF_DEBUG_HARDFAULT 0
+
+#endif
---
+++ src/example.cpp
@@ -0,0 +1,15 @@
+#include <ch32fun.h>
+#include <ch32v20xhw.h>
+
+
+int main()
+{ 
+    SystemInit();
+	
+   while (1)
+   {
+    /* code */
+   }
+   
+}
+
