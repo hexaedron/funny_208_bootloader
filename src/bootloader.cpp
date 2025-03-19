28d57972210f520dd5e8c194cd0d89f4821d7354
Konstantin Gorbenko <kyb.6.granei@yandex.ru>
2025-03-20 00:15:10 +0300

    It works now!!!

--- src/bootloader.cpp
+++ src/bootloader.cpp
@@ -9,12 +9,10 @@
 
 extern "C" void __libc_init_array(void);
 
-#define FLASH_WAIT() while(!((FLASH->STATR & FLASH_FLAG_BANK1_BSY) == FLASH_FLAG_BSY)) {}
+#define FLASH_WAIT() while(FLASH->STATR & SR_BSY) {}
 
 void eraseFlash(uint32_t start, uint32_t length)
-{
-    static bool flag = true;
-    
+{   
     /* Authorize the FPEC of Bank1 Access */
     FLASH->KEYR = FLASH_KEY1;
     FLASH->KEYR = FLASH_KEY2;
@@ -23,16 +21,18 @@ void eraseFlash(uint32_t start, uint32_t length)
     FLASH->MODEKEYR = FLASH_KEY1;
     FLASH->MODEKEYR = FLASH_KEY2;
 
+    FLASH_WAIT();
+
     // Clear flash by 256 byte pages
-    for(uint32_t addr = start; addr < start + length; addr += 256)
+    for(uint32_t addr = start; addr < (start + length); addr += 256)
     {
        FLASH->CTLR = CR_PAGE_ER;
-       FLASH->ADDR = addr;
+       FLASH->ADDR = (intptr_t)addr;
        FLASH->CTLR = CR_STRT_Set | CR_PAGE_ER;
-       while(FLASH->STATR & SR_BSY) {}
-       FLASH->CTLR &= ~CR_PAGE_ER;
+       FLASH_WAIT();
+       //FLASH->CTLR &= ~CR_PAGE_ER;
 
-       if(flag)
+       if((addr / 2048) % 2)
         {
             funDigitalWrite(PC0, FUN_HIGH);
             funDigitalWrite(PC1, FUN_HIGH);
@@ -42,7 +42,6 @@ void eraseFlash(uint32_t start, uint32_t length)
             funDigitalWrite(PC0, FUN_LOW);
             funDigitalWrite(PC1, FUN_LOW);
         }
-        flag = !flag;
     }
 
     // Lock flash back
@@ -53,17 +52,17 @@ void eraseFlash(uint32_t start, uint32_t length)
 void writeFlashWord(uint32_t addr, uint32_t data)
 {
     // Wait until not busy
-    while(FLASH->STATR & SR_BSY) {}
+    FLASH_WAIT();
 
     FLASH->CTLR |= CR_PG_Set;
 
     // Write first half
     *(__IO uint16_t *)addr = (uint16_t)data;
-    while(FLASH->STATR & SR_BSY) {}
+    FLASH_WAIT();
 
     // Write second half
     *(__IO uint16_t *)(addr + 2) = data >> 16;
-    while(FLASH->STATR & SR_BSY) {}
+    FLASH_WAIT();
 
     FLASH->CTLR &= CR_PG_Reset;
 }
@@ -71,15 +70,6 @@ void writeFlashWord(uint32_t addr, uint32_t data)
 // Write 256 byte page
 void writeFlash256(uint32_t pageAddress, uint32_t* data)
 {   
-    /* Authorize the FPEC of Bank1 Access */
-    FLASH->KEYR = FLASH_KEY1;
-    FLASH->KEYR = FLASH_KEY2;
-
-    /* Fast mode unlock */
-    FLASH->MODEKEYR = FLASH_KEY1;
-    FLASH->MODEKEYR = FLASH_KEY2;
-
-    
     FLASH->CTLR |= CR_PAGE_PG;
     while(FLASH->STATR & SR_BSY);
     while(FLASH->STATR & SR_WR_BSY);
@@ -98,10 +88,6 @@ void writeFlash256(uint32_t pageAddress, uint32_t* data)
     FLASH->CTLR |= CR_PG_STRT;
     while(FLASH->STATR & SR_BSY);
     FLASH->CTLR &= ~CR_PAGE_PG;
-
-    // Lock flash back
-    FLASH->CTLR |= CR_FAST_LOCK_Set;
-    FLASH->CTLR |= CR_LOCK_Set;
 }
 
 //Copy new to main by 256 byte pages
@@ -143,8 +129,6 @@ void copyNewToMain256(uint32_t mainStart, uint32_t newStart, uint32_t length)
 //Copy new to main by words
 void copyNewToMain(uint32_t mainStart, uint32_t newStart, uint32_t length)
 {
-    static bool flag = true;
-
     /* Authorize the FPEC of Bank1 Access */
     FLASH->KEYR = FLASH_KEY1;
     FLASH->KEYR = FLASH_KEY2;
@@ -155,7 +139,7 @@ void copyNewToMain(uint32_t mainStart, uint32_t newStart, uint32_t length)
         writeFlashWord(addr, *(uint32_t*)newAddr);
         newAddr += 4;
 
-        if(flag)
+        if((addr / 1024) % 2)
         {
             funDigitalWrite(PC0, FUN_HIGH);
             funDigitalWrite(PC1, FUN_HIGH);
@@ -165,7 +149,6 @@ void copyNewToMain(uint32_t mainStart, uint32_t newStart, uint32_t length)
             funDigitalWrite(PC0, FUN_LOW);
             funDigitalWrite(PC1, FUN_LOW);
         }
-        flag = !flag;
     }
 
     // Lock flash back
@@ -176,26 +159,26 @@ int main()
 { 
     SystemInit();
 
-    //SysTick->SR   = 0;
-    //SysTick->CMP  = DELAY_MS_TIME; // 1 ms
-    //SysTick->CNT  = 0; 
-    //SysTick->CTLR |= SYSTICK_CTLR_STE | SYSTICK_CTLR_STIE | SYSTICK_CTLR_STCLK ;
+    SysTick->SR   = 0;
+    SysTick->CMP  = DELAY_MS_TIME; // 1 ms
+    SysTick->CNT  = 0; 
+    SysTick->CTLR |= SYSTICK_CTLR_STE | SYSTICK_CTLR_STIE | SYSTICK_CTLR_STCLK ;
 //
     funGpioInitC();
     funPinMode(PC0, GPIO_Speed_2MHz | GPIO_CNF_OUT_PP);
 	funPinMode(PC1, GPIO_Speed_2MHz | GPIO_CNF_OUT_PP);
-    //Delay_Ms(3000);
+    Delay_Ms(3000);
 
     volatile uint32_t* rd = (uint32_t*)NEW_FW_ADDR;
-    //if(*rd != 0xe339e339)
+    if(*rd != 0xe339e339)
     {
-        //funDigitalWrite(PC1, FUN_HIGH);
-        //funDigitalWrite(PC0, FUN_HIGH);
+        funDigitalWrite(PC1, FUN_HIGH);
+        funDigitalWrite(PC0, FUN_HIGH);
         //Delay_Ms(500);
-        //eraseFlash(MAIN_CODE_FLASH_ADDR, NEW_FW_LENGTH);
-        //copyNewToMain(MAIN_CODE_FLASH_ADDR, NEW_FW_ADDR, NEW_FW_LENGTH);
-        //eraseFlash(NEW_FW_ADDR, NEW_FW_LENGTH);
-        //PFIC->SCTLR = 1<<31; // reboot
+        eraseFlash(MAIN_CODE_FLASH_ADDR, NEW_FW_LENGTH);
+        copyNewToMain(MAIN_CODE_FLASH_ADDR, NEW_FW_ADDR, NEW_FW_LENGTH);
+        eraseFlash(NEW_FW_ADDR, NEW_FW_LENGTH);
+        PFIC->SCTLR = 1<<31; // reboot
     }
     //eraseFlash(MAIN_CODE_FLASH_ADDR, NEW_FW_LENGTH);
 
